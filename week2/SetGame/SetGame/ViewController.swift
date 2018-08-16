f8cfa6fa3ad7dd247f33a2b8eac299e19ac1a9af
Cong Doan <cong.doan@gmail.com>
2018-08-16 10:09:52 +0700

    Initial Xcode project for Set Game

---
+++ week2/SetGame/SetGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SetGame
+//
+//  Created by Cong Doan on 8/16/18.
+//  Copyright © 2018 Cong Doan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
