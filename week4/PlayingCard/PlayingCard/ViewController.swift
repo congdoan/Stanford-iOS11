bb5c565c88c28962c42e476aaa6215ef1b61ac44
Cong Doan <cong.doan@gmail.com>
2018-09-07 23:15:12 +0700

    Add match and mismatch animations for a pair of faced-up cards

--- week4/PlayingCard/PlayingCard/ViewController.swift
+++ week4/PlayingCard/PlayingCard/ViewController.swift
@@ -33,29 +33,75 @@ class ViewController: UIViewController {
     }
     
     private var facedUpCardViews: [PlayingCardView] {
-        return cardViews.filter { $0.isFaceUp }
+        return cardViews.filter { $0.isFaceUp && !$0.isHidden }
+    }
+    
+    private var are2FacedUpCardsMatched: Bool {
+        return facedUpCardViews[0].rank == facedUpCardViews[1].rank
+            && facedUpCardViews[0].suit == facedUpCardViews[1].suit
     }
     
     @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
         if sender.state != .ended { return }
         let cardView = sender.view as! PlayingCardView
-        UIView.transition(with: cardView,
-                          duration: 0.6,
-                          options: [cardView.isFaceUp ? .transitionFlipFromLeft : .transitionFlipFromRight],
-                          animations: {
-                                cardView.isFaceUp = !cardView.isFaceUp
-                          },
-                          completion: { finished in
-                                if self.facedUpCardViews.count != 2 { return }
-                                self.facedUpCardViews.forEach { facedUpCardView in
-                                    UIView.transition(with: facedUpCardView,
-                                                      duration: 0.6,
-                                                      options: [.transitionFlipFromLeft],
-                                                      animations: {
-                                                            facedUpCardView.isFaceUp = false
-                                                      })
-                                }
-                          })
+        UIView.transition(
+            with: cardView,
+            duration: 0.6,
+            options: [cardView.isFaceUp ? .transitionFlipFromLeft : .transitionFlipFromRight],
+            animations: {
+                cardView.isFaceUp = !cardView.isFaceUp
+            },
+            completion: { finished in
+                if self.facedUpCardViews.count == 2 {
+                    self.animate2FacedUpCardViews()
+                }
+            })
+    }
+    
+    private func animate2FacedUpCardViews() {
+        if are2FacedUpCardsMatched {
+            animateMatchedPair()
+        } else {
+            animateMismatchedPair()
+        }
+    }
+    
+    private func animateMatchedPair() {
+        // Scale back down, then fade out & finally be removed from the super view
+        facedUpCardViews.forEach { view in
+            // Scale up
+            UIViewPropertyAnimator.runningPropertyAnimator(
+                withDuration: 0.6,
+                delay: 0,
+                animations: {
+                    view.transform = CGAffineTransform.identity.scaledBy(x: 3, y: 3)
+                },
+                completion: { position in
+                    // Scale back down
+                    UIViewPropertyAnimator.runningPropertyAnimator(
+                        withDuration: 0.5,
+                        delay: 0.3,
+                        animations: {
+                            view.transform = CGAffineTransform.identity.scaledBy(x: 0.1, y: 0.1)
+                        },
+                        completion: { (position) in
+                            // Hide
+                            view.isHidden = true
+                        })
+                })
+        }
+    }
+    
+    private func animateMismatchedPair() {
+        facedUpCardViews.forEach { facedUpCardView in
+            UIView.transition(
+                with: facedUpCardView,
+                duration: 0.6,
+                options: [.transitionFlipFromLeft],
+                animations: {
+                    facedUpCardView.isFaceUp = false
+                })
+        }
     }
     
 }
