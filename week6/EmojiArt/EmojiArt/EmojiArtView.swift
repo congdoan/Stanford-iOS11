1bffddc4cbcb6c7cef192e3132ba57fe5f86b580
Cong Doan <cong.doan@gmail.com>
2018-12-18 19:00:05 +0700

    Implement Dropping 'NSAttributedString' items into the EmojiArtView

--- week6/EmojiArt/EmojiArt/EmojiArtView.swift
+++ week6/EmojiArt/EmojiArt/EmojiArtView.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class EmojiArtView: UIView {
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        addInteraction(UIDropInteraction(delegate: self))
+    }
+    
     var backgroundImage: UIImage? {
         didSet { setNeedsDisplay() }
     }
@@ -18,4 +32,35 @@ class EmojiArtView: UIView {
         backgroundImage?.draw(in: bounds)
     }
 
+}
+
+extension EmojiArtView: UIDropInteractionDelegate {
+    
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSAttributedString.self)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        let dropPoint = session.location(in: self)
+        session.loadObjects(ofClass: NSAttributedString.self) { providers in
+            for attributedString in (providers as? [NSAttributedString] ?? []) {
+                self.addLabel(with: attributedString, centeredAt: dropPoint)
+            }
+        }
+    }
+    
+    private func addLabel(with attributedString: NSAttributedString, centeredAt point: CGPoint) {
+        let label = UILabel()
+        label.backgroundColor = .clear
+        label.attributedText = attributedString
+        label.sizeToFit()
+        label.center = point
+        addEmojiArtGestureRecognizers(to: label)
+        addSubview(label)
+    }
+    
 }
